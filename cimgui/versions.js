388c1e9005d8f9555e25dbd3b27de3edbcabf017
JamesWrigley <james@puiterwijk.org>
2024-12-23 01:31:26 +0100

    Aggregate documentation

---
+++ cimgui/versions.js
@@ -0,0 +1,24 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v4.0",
+  "v3.1",
+  "v3.0",
+  "v2.3",
+  "v2.2",
+  "v2.1",
+  "v2.0",
+  "v1.89",
+  "v1.82",
+  "v1.79",
+  "v1.78",
+  "v1.77",
+  "v1.76",
+  "v1.74",
+  "v1.73",
+  "v1.72",
+  "v1.69",
+  "v1.68",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v4.0.0";
+var DOCUMENTER_STABLE = "stable";
